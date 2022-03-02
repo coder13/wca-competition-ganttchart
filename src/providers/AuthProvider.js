6b5ce372439835212f798160d11758c7b7d992eb
Caleb <choover11@gmail.com>
2022-03-02 06:59:22 -0800

    properly signs out when access token expires

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { WCA_ORIGIN, WCA_OAUTH_CLIENT_ID } from '../lib/wca-env';
 import history from '../lib/history';
 
-const localStorageKey = key => `groups.${WCA_OAUTH_CLIENT_ID}.${key}`;
+const localStorageKey = key => `wca_gantt_chart.${WCA_OAUTH_CLIENT_ID}.${key}`;
 
 const getLocalStorage = (key) => localStorage.getItem(localStorageKey(key));
 const setLocalStorage = (key, value) => localStorage.setItem(localStorageKey(key), value);
@@ -22,35 +22,73 @@ const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [accessToken, setAccessToken] = useState(getLocalStorage('accessToken'));
+  const [expirationTime, setExpirationTime] = useState(getLocalStorage('expirationTime')); // Time at which it expires
+
   const location = useLocation();
 
-  useEffect(() => {
-    setLocalStorage('accessToken', accessToken);
-  }, [accessToken]);
+  const signOutIfExpired = useCallback(() => {
+    if (expirationTime && new Date() >= new Date(expirationTime)) {
+      signOut();
+      return true;
+    }
+  }, [expirationTime]);
+
+  const signIn = () => {
+    const params = new URLSearchParams({
+      client_id: WCA_OAUTH_CLIENT_ID,
+      response_type: 'token',
+      redirect_uri: oauthRedirectUri(),
+      scope: 'public manage_competitions email',
+      state: 'foobar'
+    });
+    window.location = `${WCA_ORIGIN}/oauth/authorize?${params.toString()}`;
+  };
+
+  const signOut = () => {
+    console.log('signing out')
+    setAccessToken(null);
+    setExpirationTime(null);
+    localStorage.removeItem(localStorageKey('accessToken'));
+    localStorage.removeItem(localStorageKey('expirationTime'));
+  };
 
   useEffect(() => {
-    console.log(27, location);
-    const hash = window.location.hash.replace(/^#/, '');
+    if (!expirationTime) {
+      return;
+    }
+
+
+    if (signOutIfExpired()) {
+      return;
+    }
+
+    const expiresInMillis = (new Date(expirationTime) - Date.now()) + 1000;
+
+    const timeout = setTimeout(() => {
+      signOutIfExpired();
+    }, expiresInMillis);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [expirationTime, signOutIfExpired]);
+
+  useEffect(() => {
+    const hash = location.hash.replace(/^#/, '');
     const hashParams = new URLSearchParams(hash);
 
     if (hashParams.has('access_token')) {
       setAccessToken(hashParams.get('access_token'));
+      setLocalStorage('accessToken', hashParams.get('access_token'));
     }
 
     if (hashParams.has('expires_in')) {
       /* Expire the token 15 minutes before it actually does,
          this way it doesn't expire right after the user enters the page. */
       const expiresInSeconds = hashParams.get('expires_in') - 15 * 60;
-      const expirationTime = new Date(
+      setLocalStorage('expirationTime', (new Date(
         new Date().getTime() + expiresInSeconds * 1000
-      );
-      setLocalStorage('expirationTime', expirationTime.toISOString());
-    }
-
-    /* If the token expired, sign the user out. */
-    const expirationTime = getLocalStorage('expirationTime');
-    if (expirationTime && new Date() >= new Date(expirationTime)) {
-      signOut();
+      )).toISOString());
     }
 
     /* Clear the hash if there is a token. */
@@ -59,20 +97,9 @@ export default function AuthProvider({ children }) {
     }
   }, [location]);
 
-  const signIn = (newUser, callback) => {
-    const params = new URLSearchParams({
-      client_id: WCA_OAUTH_CLIENT_ID,
-      response_type: 'token',
-      redirect_uri: oauthRedirectUri(),
-      scope: 'public manage_competitions email',
-    });
-    window.location = `${WCA_ORIGIN}/oauth/authorize?${params.toString()}`;
-  };
-
-  const signOut = (callback) => {
-    setAccessToken(null);
-    localStorage.removeItem(localStorageKey('accessToken'));
-  };
+  useEffect(() => () => {
+    signOutIfExpired();
+  }, [signOutIfExpired])
 
   const signedIn = useCallback(() => !!accessToken, [accessToken]);
 
